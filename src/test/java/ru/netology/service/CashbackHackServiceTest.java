eeadd865e69e637346d1a6e2a93ef3dfb2f5cec5
Mariya Negretskaya <marian.93@mail.ru>
2022-07-16 22:21:04 +0300

    Commit testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,48 @@
 package ru.netology.service;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int amount = 900;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 800;
+        int amount = 1200;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+
+    @Test
+    public void testRemain5() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int amount = 999;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
 
 }
